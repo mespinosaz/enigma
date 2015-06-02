b33b6909c56a379961224eb52756ea66daac5d66
Marc Espinosa <marespinosa@ebay.com>
2015-06-02 13:04:58 +0200

    Bit of refactor

--- src/Enigma.hs
+++ src/Enigma.hs
@@ -1,6 +1,7 @@
 module Enigma where
 
 import Data.Char
+import Data.Tuple
 
 rotateLeft :: Eq a => [a] -> Int -> [a]
 rotateLeft xs 0 = xs
@@ -8,52 +9,41 @@ rotateLeft (x:xs) n = rotateLeft (xs ++ [x]) (n-1)
 
 rotateRight :: Eq a => [a] -> Int -> [a]
 rotateRight xs 0 = xs
-rotateRight xs n = rotateRight ([last xs] ++ init xs) (n-1)
+rotateRight xs n = rotateRight ((last xs) : (init xs)) (n-1)
 
 rotate :: Eq a => [a] -> Int -> [a]
 rotate xs n     | n < 0 = rotateRight xs (abs n)
                 | otherwise = rotateLeft xs n
 
 rotor :: ([Char], [Char]) -> Char -> Char
-rotor ([],_) a = a
+rotor ([],_) a = a
 rotor (x:xs, y:ys) a    | a == x = y
                         | otherwise = rotor (xs, ys) a
 
 transform :: [([Char], [Char])] -> Char -> Char
-transform [] a = a
-transform (x:xs) a = transform xs (rotor x a)
-
-reverseRotor :: ([Char], [Char]) -> ([Char], [Char])
-reverseRotor (a, b) = (b, a)
+transform (xs) a = foldr rotor a (reverse xs)
 
 reverseRotors :: [([Char], [Char])] -> [([Char], [Char])]
-reverseRotors [] = []
-reverseRotors (r:rs) = reverseRotors rs ++ [reverseRotor r]
+reverseRotors rs = map swap (reverse rs)
 
 rotateFirstRotor :: [Int] -> [Int]
 rotateFirstRotor (offset:rs) = (offset + 1) : rs
 
 buildRotor :: Int -> ([Char],[Char])
-buildRotor offset = (['A'..'Z'], rotate ['A'..'Z'] offset)
+buildRotor offset = buildWheel (rotate ['A'..'Z'] offset)
 
 buildRotors :: [Int] -> [([Char],[Char])]
-buildRotors [] = []
-buildRotors (r:rs) = buildRotor r : buildRotors rs
+buildRotors rs = map buildRotor rs
 
-buildPlugBoard :: [Char] -> ([Char], [Char])
-buildPlugBoard pb = (['A'..'Z'], pb)
-
-buildReflector :: [Char] -> ([Char], [Char])
-buildReflector ref = (['A'..'Z'], ref)
+buildWheel :: [Char] -> ([Char], [Char])
+buildWheel xs = (['A'..'Z'], xs)
 
 buildEnigmaTransformations :: [Char] -> [Int] -> [Char] -> [([Char], [Char])]
-buildEnigmaTransformations pb rs ref = [buildPlugBoard pb]
-                                ++ (buildRotors rs)
-                                ++ [buildReflector ref]
-                                ++ (reverseRotors (buildRotors rs))
-                                ++ [reverseRotor (buildPlugBoard pb)]
+buildEnigmaTransformations pb rs ref = (buildWheel pb) : (buildRotors rs)
+                                ++ [buildWheel ref]
+                                ++ (reverseRotors ((buildWheel pb) : (buildRotors rs)))
 
 enigma :: [Char] -> [Int] -> [Char] -> [Char] -> [Char]
 enigma _ _ _ [] = []
-enigma pb rs ref (s:ss) =  [transform (buildEnigmaTransformations pb rs ref) (toUpper s)]
-                            ++ enigma pb (rotateFirstRotor rs) ref ss
+enigma pb rs ref (s:ss) =  transform (buildEnigmaTransformations pb rs ref) (toUpper s)
+                            : enigma pb (rotateFirstRotor rs) ref ss
