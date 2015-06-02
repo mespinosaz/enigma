baf60840bc0974be98675a4801d8f620cd612b18
Marc Espinosa <marespinosa@ebay.com>
2015-06-02 11:46:17 +0200

    Better descriptive text on test

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -5,8 +5,8 @@ main :: IO ()
 main = hspec $ do
     describe "enigma function" $ do
         it "returns encoded value" $ do
-            enigma "VZBRGITYUPSDNHLXAWMJQOFECK" [1, 88 ,3] "YRUHQSLDPXNGOKMIEBFZCWVJAT" "THIS TEXT WILL BE ENCODED"
-            `shouldBe` ("AAPU CXHR TQNJ DT FWJGNUA" :: [Char])
+            enigma "VZBRGITYUPSDNHLXAWMJQOFECK" [1, 88 ,3] "YRUHQSLDPXNGOKMIEBFZCWVJAT" "THIS TEXT WILL BE ENCODED AND DECODED"
+            `shouldBe` ("AAPU CXHR TQNJ DT FWJGNUA TEM WQYUVZZ" :: [Char])
         it "returns decoded value" $ do
-            enigma "VZBRGITYUPSDNHLXAWMJQOFECK" [1, 88 ,3] "YRUHQSLDPXNGOKMIEBFZCWVJAT" "AAPU CXHR TQNJ DT FWJGNUA"
-            `shouldBe` ("THIS TEXT WILL BE ENCODED" :: [Char])
+            enigma "VZBRGITYUPSDNHLXAWMJQOFECK" [1, 88 ,3] "YRUHQSLDPXNGOKMIEBFZCWVJAT" "AAPU CXHR TQNJ DT FWJGNUA TEM WQYUVZZ"
+            `shouldBe` ("THIS TEXT WILL BE ENCODED AND DECODED" :: [Char])
